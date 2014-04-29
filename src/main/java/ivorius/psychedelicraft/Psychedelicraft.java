bb3585428457fa7912cbfec084ad18574be702c1
Ivorius <lukastenbrink@googlemail.com>
2014-04-29 23:53:00 +0200

    Release commit - 1.4.1

--- src/main/java/ivorius/psychedelicraft/Psychedelicraft.java
+++ src/main/java/ivorius/psychedelicraft/Psychedelicraft.java
@@ -29,7 +29,7 @@ import org.apache.logging.log4j.Logger;
 public class Psychedelicraft
 {
     public static final String MODID = "psychedelicraft";
-    public static final String VERSION = "1.4";
+    public static final String VERSION = "1.4.1";
 
     @Instance(value = "psychedelicraft")
     public static Psychedelicraft instance;
